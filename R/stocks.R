a1b82a41eebb6965da1f9ec35323dfdf29f6a34a
Philippe Rast <rast.ph@gmail.com>
2020-05-18 19:23:09 -0700

    added stocks file and description

---
+++ R/stocks.R
@@ -0,0 +1,13 @@
+#' Daily data on returns of Toyota, Nissan, and Honda stocks.
+#'
+#' A dataset used by Stata to illustrate MGARCH models containing daily data on returns of Toyota, Nissan, and Honda stocks.
+#'
+#' @format Data frame with 2015 rows and 5 variables:
+#' \describe{
+#'   \item{date}{Date}
+#'   \item{t}{Sequential time index}
+#'   \item{toyota}{Daily returns for Toyota stock}
+#'   \item{nissan}{Daily returns for Nissan stock}
+#'   \item{honda}{Daily returns for Honda stock}
+#' }
+"stocks"
