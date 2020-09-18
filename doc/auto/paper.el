8a4d2f43889dcb5c78b6ed12ced1c376ab2a0f5a
Philippe Rast <rast.ph@gmail.com>
2020-09-17 21:18:41 -0700

    paper

---
+++ doc/auto/paper.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook
+ "paper"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Bollerslev1986"
+    "Bollerslev1990"
+    "Buerkner2020"
+    "Carnero2014"
+    "DeAlmeida2018"
+    "Engle1982"
+    "Engle2002"
+    "Engle1995"
+    "Rast2020"
+    "Tsay2013"
+    "Yao2018"))
+ :bibtex)
+
