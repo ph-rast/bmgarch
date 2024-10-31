c6f81d8867527fe246accb6624878d2ebf04563b
Philippe Rast <rast.ph@gmail.com>
2024-10-31 13:09:22 -0700

    *optional* presence for cmdstanr package is checked - only throw warning

--- R/bmgarch-package.R
+++ R/bmgarch-package.R
@@ -1,6 +1,6 @@
 #' The 'bmgarch' package. 
 #' 
-#' @description The *bmgarch* package fits Bayesian multivariate GARCH models specified via stan,
+#' @description The *bmgarch* package fits Bayesian multivariate GARCH models specified via Stan,
 #' a C++ package providing HMC methods for full Bayesian inference (cf. [http://mc-stan.org]). The currently implemented parameterizations are DCC(Q,P),
 #' CCC(Q,P), and BEKK(Q,P) with arbitrary lags defined in Q, and P. The package provides summaries and plots for the estimates as well
 #' as forecasted series with corresponding plots. The fitted objects are rstan class objects that can be inspected and manipulated
@@ -8,7 +8,7 @@
 #'
 #' @author Philippe Rast
 #' 
-#' @docType package
+#' @keywords internal
 #' @name bmgarch-package
 #' @useDynLib bmgarch, .registration = TRUE
 #' @import methods
@@ -16,8 +16,7 @@
 #' @import rstantools
 #' @importFrom rstan sampling
 #' 
-#' 
 #' @references 
 #' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
 #' 
-NULL
+"_PACKAGE"
--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,17 @@
 .onLoad <- function(libname, pkgname) {
   modules <- paste0("stan_fit4", names(stanmodels), "_mod")
   for (m in modules) loadModule(m, what = TRUE)
+
+  ## set core options
   op = options()
   options(mc.cores = parallel::detectCores())
-    ## Check for presence of cmdstanr
-  if( !require("cmdstanr") ) stop("The `cmdstanr` library as well as CmdStan are required to run this pacakge. Check out https://mc-stan.org/cmdstanr")
+  
+  ## Check for optional presence of cmdstanr
+  if (!requireNamespace("cmdstanr", quietly = TRUE)) {
+    packageStartupMessage(
+      "The `cmdstanr` package is not installed. Some functionality may be limited. ",
+      "To fully use `bmgarch`, consider installing `cmdstanr` from https://mc-stan.org/cmdstanr"
+    )
+  }
 }
   
